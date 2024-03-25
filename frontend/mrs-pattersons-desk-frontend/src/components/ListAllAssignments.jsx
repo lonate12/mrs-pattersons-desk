928ef91593f2e4eb6a3c001a31f0cb8a758ca511
Rene Onate <lonate12@gmail.com>
2024-03-25 11:44:55 -0400

    Implemented assignment list sorting and display into cards via AssignmentCard component in the ListAll Assignments file

--- frontend/mrs-pattersons-desk-frontend/src/components/ListAllAssignments.jsx
+++ frontend/mrs-pattersons-desk-frontend/src/components/ListAllAssignments.jsx
@@ -2,6 +2,9 @@ import { AuthData } from "../auth/AuthWrapper";
 import { useState, useEffect } from "react";
 import { getAllAssignments } from "../helpers/apiCalls";
 import AssignmentListItem from "./AssignmentListItem";
+import { Link } from "react-router-dom";
+import assignmentNames from "../helpers/assignmentNames";
+import PropTypes from 'prop-types'
 
 export default function ListAllAssignments() {
 
@@ -10,53 +13,21 @@ export default function ListAllAssignments() {
 
     useEffect(() => {
         getAllAssignments(user.token).then((response) => {
+            console.log(response);
             setAssignments(response);
         });
     }, [user]);
 
-    const assignmentListItems = assignments.map((assignment) => <AssignmentListItem key={assignment.id}
-                    id={assignment.id} 
-                    number={assignment.number} 
-                    status={assignment.status} 
-                    githubUrl={assignment.githubUrl} 
-                    branch={assignment.branch} 
-                />
-        );
-        
+    const mapOfAssignmentCardLists = filterAndMapAssignments(assignments);
 
     return (
         <>
-            {/* <div className="row bg-light col-10 offset-1" style={{marginTop: 20, marginBottom: 20, paddingBottom: 20}}>
+            <div className="row bg-light col-10 offset-1" style={{marginTop: 20, marginBottom: 20, paddingBottom: 20}}>
                 <h1 className="text-center pt-3">Dashboard for {user.name}</h1>
                 <button className="btn btn-primary col-lg-6 offset-lg-3 col-10 offset-1">Submit new assignment</button>
-                <div className="container">
-                    <div className="boarder boarder-primary form-control mt-5 mb-3 position-relative">
-                        <span className="badge rounded-pill bg-success position-absolute" style={{left: 20, top: -15, fontSize: 20}}>Submitted</span>
-                        <p className="mt-3">test</p>
-                    </div>
-                    <div className="boarder boarder-primary form-control mt-5 mb-3 position-relative">
-                        <span className="badge rounded-pill bg-secondary position-absolute" style={{left: 20, top: -15, fontSize: 20}}>In Review</span>
-                        <p className="mt-3">test</p>
-                    </div>                    
-                    <div className="boarder boarder-primary form-control mt-5 mb-3 position-relative">
-                        <span className="badge rounded-pill bg-danger position-absolute" style={{left: 20, top: -15, fontSize: 20}}>Needs Rework</span>
-                        <p className="mt-3">test</p>
-                    </div>
-                </div>
-                <div className="container">
-                    <div className="boarder boarder-primary form-control mt-3 mb-3 position-relative">
-                        <span className="badge rounded-pill bg-primary position-absolute" style={{left: 20, top: -15, fontSize: 20}}>Completed</span>
-                        <p className="mt-3">test</p>
-                    </div>
-                </div>
-            </div>
-            <div className="row">
-                <p>Username: {user.name}</p>
-                { assignmentListItems ? <ul className="list-group">{assignmentListItems}</ul> : null }
-            </div> */}
             
-            <div className="container bg-light mt-3 mb-3 rounded">
-                <div className="row">
+                <div className="container">
+                <div className="row rounded">
                 <ul className="nav nav-tabs" id="myTab" role="tablist">
                     <li className="nav-item" role="presentation">
                         <button className="nav-link active" id="wip-tab" data-bs-toggle="tab" data-bs-target="#wip-tab-pane" type="button" role="tab" aria-controls="wip-tab-pane" aria-selected="true">WIP</button>
@@ -69,27 +40,73 @@ export default function ListAllAssignments() {
                     <div className="tab-pane fade show active " id="wip-tab-pane" role="tabpanel" aria-labelledby="wip-tab" tabIndex="0">
                         <div className="boarder boarder-primary form-control mt-5 mb-3 position-relative">
                             <span className="badge rounded-pill bg-success position-absolute" style={{left: 20, top: -15, fontSize: 20}}>Submitted</span>
-                            <p className="mt-3">test</p>
+                            {"SUBMITTED" in mapOfAssignmentCardLists ? mapOfAssignmentCardLists.SUBMITTED : <h3 className="mt-4">No assignments to view</h3>}
+                            {"RESUBMITTED" in mapOfAssignmentCardLists ? mapOfAssignmentCardLists.RESUBMITTED : <h3 className="mt-4">No assignments to view</h3>}
                         </div>
                         <div className="boarder boarder-primary form-control mt-5 mb-3 position-relative">
                             <span className="badge rounded-pill bg-secondary position-absolute" style={{left: 20, top: -15, fontSize: 20}}>In Review</span>
-                            <p className="mt-3">test</p>
+                            {"UNDER_REVIEW" in mapOfAssignmentCardLists ? mapOfAssignmentCardLists["UNDER_REVIEW"] : <h3 className="mt-4">No assignments to view</h3>}
                         </div>                    
                         <div className="boarder boarder-primary form-control mt-5 mb-3 position-relative">
                             <span className="badge rounded-pill bg-danger position-absolute" style={{left: 20, top: -15, fontSize: 20}}>Needs Rework</span>
-                            <p className="mt-3">test</p>
+                            {"REJECTED" in mapOfAssignmentCardLists ? mapOfAssignmentCardLists["REJECTED"] : <h3 className="mt-4">No assignments to view</h3>}
                         </div>
                     </div>
                     <div className="tab-pane fade" id="completed-tab-pane" role="tabpanel" aria-labelledby="completed-tab" tabIndex="0">
                         <div className="boarder boarder-primary form-control mt-5 mb-3 position-relative">
                             <span className="badge rounded-pill bg-primary position-absolute" style={{left: 20, top: -15, fontSize: 20}}>Completed</span>
-                            <p className="mt-3">test</p>
+                            {"COMPLETED" in mapOfAssignmentCardLists ? mapOfAssignmentCardLists["COMPLETED"] : <h3 className="mt-4">No assignments to view</h3>}
                         </div>
                     </div>
                     </div>
                 </div>
             </div>
+            </div>
         </>
 
     );
+}
+
+
+// Function to filter and sort different assignment statuses
+function filterAndMapAssignments(assignments) {
+    let mapOfAssignments = {};
+
+    assignments.forEach((assignment) => {
+        console.log(assignment);
+        // Make each assignment a card
+        const assignmentCard = <AssignmentCard assignment={assignment} />;
+
+        let status = assignment.status;
+
+        if (assignment.status === "RESUBMITTED") {
+            status = "SUBMITTED";
+        }
+
+        // If this is the first assignment with a given status, lets put an 
+        // empty list in that spot in the map
+        if (!(assignment.status in mapOfAssignments)) {
+            mapOfAssignments[status] = [];
+        }
+
+        mapOfAssignments[status].push(assignmentCard);
+    });
+
+    return mapOfAssignments;
+}
+
+const AssignmentCard = ({assignment}) => {
+    return (
+        <div className="card col-12 col-md-6 col-xl-3 mt-4">
+            <div className="card-body">
+                <h5 className="card-title position-relative">Assignment #{assignment.number}{assignment.status === "RESUBMITTED" ? <span className="badge text-bg-info rounded-circle position-absolute" style={{right: 0, top: -2}}>R</span> : null}</h5>
+                <p className="card-text">{assignmentNames[assignment.number]}</p>
+                <Link to={`${assignment.id}`} className="btn btn-primary">Details</Link>
+            </div>
+        </div>
+    );
+}
+
+AssignmentCard.propTypes = {
+    assignment: PropTypes.object
 }
---
+++ frontend/mrs-pattersons-desk-frontend/src/helpers/assignmentNames.js
@@ -0,0 +1,16 @@
+const assignmentNames = {
+    1: "Intro to Java",
+    2: "Intro to JavaScript",
+    3: "Intermediate Java",
+    4: "Intermediate JavaScript",
+    5: "Advanced Java",
+    6: "Advanced JavaScript",
+    7: "Spring Boot Basics",
+    8: "Spring Security",
+    9: "Spirng JPA",
+    10: "React Basics",
+    11: "React Router Basics"
+};
+
+
+export default assignmentNames;
