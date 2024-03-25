2eb9fdeab3622f8fced0c04852db210cbff24eee
Rene Onate <lonate12@gmail.com>
2024-03-24 23:03:25 -0400

    Reworked all assignments display to be a tab layout with WIP assignments in one tab and completed assignments in the other

--- frontend/mrs-pattersons-desk-frontend/src/components/AssignmentDetail.jsx
+++ frontend/mrs-pattersons-desk-frontend/src/components/AssignmentDetail.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { AuthData } from "../auth/AuthWrapper";
 import { isEmpty } from "../helpers/utils";
@@ -6,30 +6,62 @@ import { getAssignment } from "../helpers/apiCalls";
 
 export default function AssignmentDetail() {
     const {assignmentId} = useParams();
-    const [assignment, setAssignment] = useState({});
+    const [pageData, setPageData] = useState({assignment: {}, formData: {}});
+    const [enableEdit, setEnableEdit] = useState(false);
     const { user } = AuthData();
 
     useEffect(() => {
         getAssignment(user.token, assignmentId).then((res) => {
             console.log(res);
-            setAssignment(res);
+            setPageData({assignment: res, formData: res});
         })
-    }, []);
+    }, [assignmentId, user]);
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        const updatedFormData = {...pageData.formData, [name]: value};
+        setPageData((prevPageData) => ({...prevPageData, formData: updatedFormData}));
+    }
 
 
-    if (isEmpty(assignment)) {
+    if (isEmpty(pageData.assignment)) {
         return (
-            <div className="row bg-light col-10 offset-1">
+            <div className="row bg-light col-10 offset-1 mt-3">
                 <h1>Nothing to see here</h1>
             </div>
         );
     } else {
+        const submitted = (["SUBMITTED", "RESUBMITTED"].includes(pageData.assignment.status)  && !enableEdit) ? true : false;
+
         return (
-            <div className="row bg-light col-10 offset-1">
-                <h1>Assignment {assignment.id}</h1>
+            <div className="row bg-light col-10 offset-1 mt-3" style={{marginTop: 20}}>
+                <h1 className="text-center pt-3">Assignment #{pageData.assignment.number} <span className="badge text-bg-secondary rounded-pill">{pageData.assignment.status}</span></h1>
                 <div className="row">
-                    <h4>Status: {assignment.status}</h4>
-                    <p>Assignment #{assignment.number}</p>
+                    <form>
+                        <fieldset disabled={submitted}>
+                            <div className="mb-3">
+                                <label htmlFor="githubUrl" className="form-label">GitHub URL</label>
+                                <input type="text" className="form-control" id="githubUrl" name="githubUrl" value={pageData.formData.githubUrl} onChange={handleChange}/>
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="branch" className="form-label">Branch</label>
+                                <input type="text" className="form-control" id="branch" name="branch" value={pageData.formData.branch} onChange={handleChange}/>
+                            </div>
+                            <div className="mb-3">
+                                <button className={submitted ? "btn btn-danger" : "btn btn-primary"} 
+                                    type="submit">
+                                        {submitted ? "Already submitted" : "Submit"}
+                                </button>
+                            </div>
+                        </fieldset>
+                    </form>
+                </div>
+                <div className="row">
+                    <Link to={"/assignments"} 
+                                    className="btn btn-secondary mb-3" 
+                                    style={{marginLeft: 10}}>
+                                        Back to Assignments
+                    </Link>
                 </div>
             </div>
         );
--- frontend/mrs-pattersons-desk-frontend/src/components/ListAllAssignments.jsx
+++ frontend/mrs-pattersons-desk-frontend/src/components/ListAllAssignments.jsx
@@ -25,10 +25,71 @@ export default function ListAllAssignments() {
         
 
     return (
-        <div className="row">
-            <h1>This is the assignments page</h1>
-            <p>Username: {user.name}</p>
-            { assignmentListItems ? <ul className="list-group">{assignmentListItems}</ul> : null }
-        </div>
+        <>
+            {/* <div className="row bg-light col-10 offset-1" style={{marginTop: 20, marginBottom: 20, paddingBottom: 20}}>
+                <h1 className="text-center pt-3">Dashboard for {user.name}</h1>
+                <button className="btn btn-primary col-lg-6 offset-lg-3 col-10 offset-1">Submit new assignment</button>
+                <div className="container">
+                    <div className="boarder boarder-primary form-control mt-5 mb-3 position-relative">
+                        <span className="badge rounded-pill bg-success position-absolute" style={{left: 20, top: -15, fontSize: 20}}>Submitted</span>
+                        <p className="mt-3">test</p>
+                    </div>
+                    <div className="boarder boarder-primary form-control mt-5 mb-3 position-relative">
+                        <span className="badge rounded-pill bg-secondary position-absolute" style={{left: 20, top: -15, fontSize: 20}}>In Review</span>
+                        <p className="mt-3">test</p>
+                    </div>                    
+                    <div className="boarder boarder-primary form-control mt-5 mb-3 position-relative">
+                        <span className="badge rounded-pill bg-danger position-absolute" style={{left: 20, top: -15, fontSize: 20}}>Needs Rework</span>
+                        <p className="mt-3">test</p>
+                    </div>
+                </div>
+                <div className="container">
+                    <div className="boarder boarder-primary form-control mt-3 mb-3 position-relative">
+                        <span className="badge rounded-pill bg-primary position-absolute" style={{left: 20, top: -15, fontSize: 20}}>Completed</span>
+                        <p className="mt-3">test</p>
+                    </div>
+                </div>
+            </div>
+            <div className="row">
+                <p>Username: {user.name}</p>
+                { assignmentListItems ? <ul className="list-group">{assignmentListItems}</ul> : null }
+            </div> */}
+            
+            <div className="container bg-light mt-3 mb-3 rounded">
+                <div className="row">
+                <ul className="nav nav-tabs" id="myTab" role="tablist">
+                    <li className="nav-item" role="presentation">
+                        <button className="nav-link active" id="wip-tab" data-bs-toggle="tab" data-bs-target="#wip-tab-pane" type="button" role="tab" aria-controls="wip-tab-pane" aria-selected="true">WIP</button>
+                    </li>
+                    <li className="nav-item" role="presentation">
+                        <button className="nav-link" id="completed-tab" data-bs-toggle="tab" data-bs-target="#completed-tab-pane" type="button" role="tab" aria-controls="completed-tab-pane" aria-selected="false">Completed</button>
+                    </li>
+                </ul>
+                <div className="tab-content container" id="myTabContent">
+                    <div className="tab-pane fade show active " id="wip-tab-pane" role="tabpanel" aria-labelledby="wip-tab" tabIndex="0">
+                        <div className="boarder boarder-primary form-control mt-5 mb-3 position-relative">
+                            <span className="badge rounded-pill bg-success position-absolute" style={{left: 20, top: -15, fontSize: 20}}>Submitted</span>
+                            <p className="mt-3">test</p>
+                        </div>
+                        <div className="boarder boarder-primary form-control mt-5 mb-3 position-relative">
+                            <span className="badge rounded-pill bg-secondary position-absolute" style={{left: 20, top: -15, fontSize: 20}}>In Review</span>
+                            <p className="mt-3">test</p>
+                        </div>                    
+                        <div className="boarder boarder-primary form-control mt-5 mb-3 position-relative">
+                            <span className="badge rounded-pill bg-danger position-absolute" style={{left: 20, top: -15, fontSize: 20}}>Needs Rework</span>
+                            <p className="mt-3">test</p>
+                        </div>
+                    </div>
+                    <div className="tab-pane fade" id="completed-tab-pane" role="tabpanel" aria-labelledby="completed-tab" tabIndex="0">
+                        <div className="boarder boarder-primary form-control mt-5 mb-3 position-relative">
+                            <span className="badge rounded-pill bg-primary position-absolute" style={{left: 20, top: -15, fontSize: 20}}>Completed</span>
+                            <p className="mt-3">test</p>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </>
+
     );
 }
